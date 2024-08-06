9651c7f03c07bc0d014c11c04607f7ff7f5a8b97
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-08-06 16:48:17 +0530

    removing zenml files

--- check.py
+++
@@ -1,5 +0,0 @@
-import mlflow
-client = mlflow.tracking.MlflowClient()
-model_versions = client.search_model_versions("name='RandomForestModel'")
-for version in model_versions:
-    print(f"version: {version.version}, stage: {version.current_stage}")
